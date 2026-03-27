bbd2f3d414031f0db978c269086a5a6d22dab067
arjunbhadauriyaa <your_email>
2026-03-28 01:01:27 +0530

    UI3

--- Functions.py
+++ Functions.py
@@ -1,1 +1,29 @@
-print("Hello World")
+# print("Hello World")
+
+
+# m1=int(input("Enter your marks:"))
+# m2=int(input("Enter your marks:"))
+# m3=int(input("Enter your marks:"))
+
+# total_percentage=((m1+m2+m3)/3)
+
+# if(total_percentage>=40 and m1>33 and m2>33 and m3>33):
+#     print("Congratulations ypu are passed")
+
+# else:
+#     print("You Failed")    
+# n=int(input("Enter your number :"))
+# product = 1
+# for i in range(1,n+1):
+#     product=product*i
+
+
+# print(product)    
+
+# n=int(input("Enter your number :"))
+   
+# sum=0
+# for i in range (1,n+1):
+#     sum=sum+i
+# print(sum)    
+
