1b9d9943d181eae80e5b4e5ce38558594791e2f2
arjunbhadauriyaa <your_email>
2026-03-29 15:58:27 +0530

    P1

---
+++ Python_Questions.py
@@ -0,0 +1,67 @@
+# import math
+# num=int(input("Enter your number :"))
+# sr=math.sqrt(num)
+# print(sr)
+
+# def area():
+#     base=int(input("Enter your Base :"))
+#     height=int(input("Enter your Height :"))
+
+#     area=0.5*base*height
+#     print(area)
+  
+
+# area() 
+# import cmath   
+# def quadratic():
+    
+#      a=int(input("Enter the value of a :"))
+#      b=int(input("Enter the value of b :"))
+#      c=int(input("Enter the value of c :"))
+
+#      d=(b**2-4*a*c)
+#      root1=(-b+cmath.sqrt(d))/(2*a)
+#      root2=(-b-cmath.sqrt(d))/(2*a)
+#      print(root1 ,"and", root2)
+
+# quadratic()     
+
+
+# import random
+# num=random.randint(0,10)
+# print(num)
+
+# year =int(input("Enter the year :"))
+# if(year%400==0)and(year%100==0):
+#     print("Leap year")
+# elif(year%4==0)and(year%100!=0):
+#     print("Leap year")
+# else:
+#     print('Not a leap year')        
+# num=int(input("Enter the number :"))
+
+# if(num==1):
+#     print("Give some valid number ")
+# elif(num>1):
+
+#     for i in range(2,num):
+#         if(num%i==0):
+#             print("Not Prime")
+#             break
+#         else:
+#             print("IT is prime")    
+
+
+# lower=int(input("Enter the lower limit :"))
+# upper=int(input("Enter the upper limit :"))
+# for num in range(lower,upper+1):
+#     if num>1:
+#         for i in range(2,num):
+#             if num%i==0:
+
+#                 break
+#         else:
+#             print(num)    
+
+
+
